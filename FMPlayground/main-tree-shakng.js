05cb0fc746740fbe88fba306ceabb728c134ac00
Joseph Simpson <joe@radicalappdev.com>
2023-09-28 06:24:21 -0400

    Switching babylon js imports and adding a tree shaking example

--- FMPlayground/main-tree-shakng.js
+++ FMPlayground/main-tree-shakng.js
@@ -1,7 +1,17 @@
-import * as BABYLON from "@babylonjs/core";
+// Core
+import { Engine } from "@babylonjs/core/Engines/engine";
+import { Scene } from "@babylonjs/core/scene";
+import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { CreateBox } from "@babylonjs/core/Meshes/Builders/boxBuilder";
+import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial";
+import { Color3 } from "@babylonjs/core/Maths/math.color";
 
-import * as GUI from "@babylonjs/gui/2D";
-// BABYLON.GUI = GUI; // assing GUI to BABYLON.GUI to match the usage in the playgrounds
+// GUI
+import { AdvancedDynamicTexture } from "@babylonjs/gui/2D/advancedDynamicTexture";
+import { TextBlock } from "@babylonjs/gui/2D/controls/textBlock";
+import { Control } from "@babylonjs/gui/2D/controls/control";
 
 console.log("main.js loaded");
 
@@ -10,27 +20,27 @@ const createScene = async () => {
   const canvas = document.getElementById("bjsCanvas");
 
   // Create the enging and scene
-  const engine = new BABYLON.Engine(canvas, true);
-  const scene = new BABYLON.Scene(engine);
+  const engine = new Engine(canvas, true);
+  const scene = new Scene(engine);
   engine.setHardwareScalingLevel(1 / window.devicePixelRatio); // used to fix the scaling issue on high DPI screens, maily mainly applies to GUI
 
   // Create a camera
-  const camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 0, BABYLON.Vector3.Zero(), scene);
-  camera.position = new BABYLON.Vector3(0, 3, -5);
+  const camera = new ArcRotateCamera("Camera", 0, 0, 0, Vector3.Zero(), scene);
+  camera.position = new Vector3(0, 3, -5);
   camera.attachControl(canvas, true); // Attach the camera controls to the canvas
-  camera.setTarget(BABYLON.Vector3.Zero()); // Target the camera to scene origin. You could also target a mesh, or something else
+  camera.setTarget(Vector3.Zero()); // Target the camera to scene origin. You could also target a mesh, or something else
   camera.useAutoRotationBehavior = true; // Remove this to stop the auto rotation
 
   // // Create a basic light
-  const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+  const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
   light.intensity = 1.2;
 
   // Create a material and a cube
-  const material = new BABYLON.StandardMaterial(scene);
+  const material = new StandardMaterial(scene);
   material.alpha = 1;
-  material.diffuseColor = BABYLON.Color3.FromHexString("#f1f5f9");
+  material.diffuseColor = Color3.FromHexString("#f1f5f9");
 
-  const cube = BABYLON.MeshBuilder.CreateBox("box", {
+  const cube = CreateBox("box", {
     height: 1,
     width: 1,
     depth: 1
@@ -39,10 +49,10 @@ const createScene = async () => {
 
   // Create a GUI
   // First, create a fullscreen UI using the AdvancedDynamicTexture
-  const advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("overlay", true, scene);
+  const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("overlay", true, scene);
 
   // Then add a textblock to the overlay.
-  const title = new GUI.TextBlock("gui-title");
+  const title = new TextBlock("gui-title");
   title.text = "FMPlayground";
   title.color = "white";
   title.fontSize = "32px";
@@ -53,8 +63,8 @@ const createScene = async () => {
   title.paddingBottom = "16px";
   title.paddingLeft = "16px";
   title.paddingRight = "16px";
-  title.textVerticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
-  title.textHorizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+  title.textVerticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
+  title.textHorizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
   advancedTexture.addControl(title);
 
   return { scene, engine };
