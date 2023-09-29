06ea9a50c0fbc53e2c50a5f55c01f88808bff299
Joseph Simpson <joe@radicalappdev.com>
2023-09-29 06:49:05 -0400

    Starting code migration for box chart

--- BoxChart/main.js
+++ BoxChart/main.js
@@ -1,7 +1,42 @@
 import * as BABYLON from "@babylonjs/core";
 import * as GUI from "@babylonjs/gui/2D";
+// TODO Import this from FileMaker
+import data from "/data/export_categories_starting.json";
 
 console.log("main.js loaded");
+
+console.log("data", data);
+
+export function findLargestValue(data) {
+  let largestValue = 0;
+
+  for (const category in data.CategorySummary) {
+    if (data.CategorySummary.hasOwnProperty(category)) {
+      const categoryData = data.CategorySummary[category];
+      const CreationCount = categoryData.CreationCount || 0;
+      const DeletionCount = categoryData.DeletionCount || 0;
+      const ModificatonCount = categoryData.ModificatonCount || 0;
+
+      const maxCount = Math.max(CreationCount, DeletionCount, ModificatonCount);
+      if (maxCount > largestValue) {
+        largestValue = maxCount;
+      }
+    }
+  }
+
+  return largestValue;
+}
+
+// Function to determine where a value falls between 0 and the largest value
+export function getValuePosition(value, largestValue) {
+  if (value < 0) {
+    return 0;
+  } else if (value > largestValue) {
+    return 1;
+  } else {
+    return value / largestValue;
+  }
+}
 
 const createScene = async () => {
   // get the canvas from the DOM
@@ -18,25 +53,42 @@ const createScene = async () => {
   camera.position = new BABYLON.Vector3(0, 3, -5);
   camera.attachControl(canvas, true); // Attach the camera controls to the canvas
   camera.setTarget(BABYLON.Vector3.Zero()); // Target the camera to scene origin. You could also target a mesh, or something else
-  camera.useAutoRotationBehavior = true; // Remove this to stop the auto rotation
-  camera.lowerRadiusLimit = 2;
-  camera.upperRadiusLimit = 10;
+  // camera.lowerRadiusLimit = 2;
+  // camera.upperRadiusLimit = 10;
 
   // // Create a basic light
   const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
   light.intensity = 1.2;
 
-  // Create a material and a cube
-  const material = new BABYLON.StandardMaterial("box-mat", scene);
-  material.alpha = 1;
-  material.diffuseColor = BABYLON.Color3.FromHexString("#94a3b8");
+  // Materials
+  const matRed = new BABYLON.StandardMaterial("matRed", scene);
+  matRed.diffuseColor = BABYLON.Color3.FromHexString("#C7303E");
+  matRed.specularColor = new BABYLON.Color3(0.2, 0.2, 0.2);
 
-  const cube = BABYLON.MeshBuilder.CreateBox("box", {
-    height: 1,
-    width: 1,
-    depth: 1
-  });
-  cube.material = material;
+  const matGreen = new BABYLON.StandardMaterial("matGreen", scene);
+  matGreen.diffuseColor = BABYLON.Color3.FromHexString("#23923C");
+  matGreen.specularColor = new BABYLON.Color3(0.2, 0.2, 0.2);
+
+  const matBlue = new BABYLON.StandardMaterial("matBlue", scene);
+  matBlue.diffuseColor = BABYLON.Color3.FromHexString("#4374d1");
+  matBlue.specularColor = new BABYLON.Color3(0.2, 0.2, 0.2);
+
+  const matWhite = new BABYLON.StandardMaterial("matWhite", scene);
+  matWhite.diffuseColor = new BABYLON.Color3(1, 1, 1);
+  matWhite.specularColor = new BABYLON.Color3(0.2, 0.2, 0.2);
+
+  // Default Boxes
+  const boxGreen = BABYLON.MeshBuilder.CreateBox("box", { size: 1 }, scene);
+  boxGreen.position = new BABYLON.Vector3(0, 0.5, 0);
+  boxGreen.material = matGreen;
+
+  const boxRed = BABYLON.MeshBuilder.CreateBox("box", { size: 1 }, scene);
+  boxRed.position = new BABYLON.Vector3(-1.5, 0.5, 0);
+  boxRed.material = matRed;
+
+  const boxBlue = BABYLON.MeshBuilder.CreateBox("box", { size: 1 }, scene);
+  boxBlue.position = new BABYLON.Vector3(1.5, 0.5, 0);
+  boxBlue.material = matBlue;
 
   // Create a GUI
   // First, create a fullscreen UI using the AdvancedDynamicTexture
@@ -44,7 +96,7 @@ const createScene = async () => {
 
   // Then add a textblock to the overlay.
   const title = new GUI.TextBlock("gui-title");
-  title.text = "FMPlayground";
+  title.text = "Box Chart";
   title.fontFamily = "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif";
   title.color = "black";
   title.fontSize = "32px";
