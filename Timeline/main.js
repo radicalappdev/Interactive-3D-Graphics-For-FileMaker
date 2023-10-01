2bcd0c5ff5ab468f5264cb7552e4af3933ca4a42
Joseph Simpson <joe@radicalappdev.com>
2023-10-01 05:50:22 -0400

    Adding Timeline project

---
+++ Timeline/main.js
@@ -0,0 +1,87 @@
+import * as BABYLON from "@babylonjs/core";
+import * as GUI from "@babylonjs/gui/2D";
+
+console.log("main.js loaded");
+
+const createScene = async () => {
+  // get the canvas from the DOM
+  const canvas = document.getElementById("bjsCanvas");
+
+  // Create the enging and scene
+  const engine = new BABYLON.Engine(canvas, true);
+  const scene = new BABYLON.Scene(engine);
+  scene.clearColor = BABYLON.Color3.FromHexString("#ffffff");
+  engine.setHardwareScalingLevel(1 / window.devicePixelRatio); // used to fix the scaling issue on high DPI screens, maily mainly applies to GUI
+
+  // Create a camera
+  const camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 0, BABYLON.Vector3.Zero(), scene);
+  camera.position = new BABYLON.Vector3(0, 3, -5);
+  camera.attachControl(canvas, true); // Attach the camera controls to the canvas
+  camera.setTarget(BABYLON.Vector3.Zero()); // Target the camera to scene origin. You could also target a mesh, or something else
+  camera.useAutoRotationBehavior = true; // Remove this to stop the auto rotation
+  camera.lowerRadiusLimit = 2;
+  camera.upperRadiusLimit = 10;
+
+  // // Create a basic light
+  const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+  light.intensity = 1.2;
+
+  // Create a material and a cube
+  const material = new BABYLON.StandardMaterial("box-mat", scene);
+  material.alpha = 1;
+  material.diffuseColor = BABYLON.Color3.FromHexString("#94a3b8");
+
+  const cube = BABYLON.MeshBuilder.CreateBox("box", {
+    height: 1,
+    width: 1,
+    depth: 1
+  });
+  cube.material = material;
+
+  // Create a GUI
+  // First, create a fullscreen UI using the AdvancedDynamicTexture
+  const advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("overlay", true, scene);
+
+  // Then add a textblock to the overlay.
+  const title = new GUI.TextBlock("gui-title");
+  title.text = "FMPlayground";
+  title.fontFamily = "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif";
+  title.color = "black";
+  title.fontSize = "32px";
+  title.fontWeight = "bold";
+  title.height = "100%";
+  title.width = "100%";
+  title.paddingTop = "20px";
+  title.paddingBottom = "16px";
+  title.paddingLeft = "16px";
+  title.paddingRight = "16px";
+  title.textVerticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
+  title.textHorizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+  advancedTexture.addControl(title);
+
+  return { scene, engine };
+};
+
+// When the DOM is ready, run the createScene function
+window.addEventListener("DOMContentLoaded", async function () {
+  const { scene, engine } = await createScene();
+  // Start the render loop
+  engine.runRenderLoop(function () {
+    scene.render();
+  });
+
+  // Resize the engine on window resize
+  window.addEventListener("resize", function () {
+    engine.resize();
+  });
+
+  // A simple function that can be called from FileMaker to make a change in the scene
+  this.window.changeBoxColor = (data) => {
+    const parsed = JSON.parse(data);
+    console.log("changeBoxColor", parsed);
+    //  get the box material by name
+    const mat = scene.getMaterialByName("box-mat");
+    // set the color
+    mat.diffuseColor = BABYLON.Color3.FromHexString(parsed.color);
+  };
+});
